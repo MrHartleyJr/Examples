fc7d66b6e63611b6c08933649fc8ab33a504c3bc
Hart Ley <mr.hartley.jr@gmail.com>
2023-10-07 17:03:47 +0500

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Input username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("УРАААА");
+    Console.WriteLine(username);
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
